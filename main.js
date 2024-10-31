c388ca4584aea5e791816fcc5f8ea6dd64bf0155
Milvookie <58269358+Milvookie@users.noreply.github.com>
2024-10-31 22:14:39 +0100

    add basic functions

---
+++ main.js
@@ -0,0 +1,37 @@
+
+
+function operate(num1, num2, symbol) {
+    switch (symbol) {
+        case '+':
+            return add(num1, num2)
+        case '-':
+            return substract(num1, num2)
+        case '*':
+            return multiply(num1, num2)
+        case '/':
+            return divide(num1, num2)
+        default:
+            break;
+    }
+}
+
+let operandOne, operandTwo, operator;
+
+function add(a, b) {
+    return a + b
+}
+
+function substract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    if (b == 0) {
+        return 'Nice try smartass'
+    }
+    return a / b
+}
